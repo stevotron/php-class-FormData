8c6b3e2a2af59d82eed9d4c15cb5cfa18a71350e
Proceed <steve@prcd.co>
2016-02-11 11:14:18 +0000

    Bug fix in number.php
    value_clean was not being set if " $step != 'any' " despite no errors being generated. The if statements have been modified to resolve the issue.

--- class/FormData/check-field/number.php
+++ class/FormData/check-field/number.php
@@ -15,48 +15,36 @@ $this->field[$name]['value'] = $input;
 
 $input = preg_replace("|\s|",'',$input);
 $input = preg_replace("|\A00+\z|", '0', $input);
-if ($input != '0')
-{
+if ($input != '0') {
 	$input = trim($input,'0');
 }
 
 $decCheck = explode('.', $input);
 
-if ($input == '' && $required == '1')
-{
+if ($input == '' && $required == '1') {
 	$this->setError($name, 'is required');
 }
-else if (!preg_match("|\A\-?[0-9]*\.?[0-9]*\z|",$input) || $input < $min || $input > $max)
-{
+else if (!preg_match("|\A\-?[0-9]*\.?[0-9]*\z|",$input) || $input < $min || $input > $max) {
 	// make error message relevant to number parameters
 	$suf = $decimal > 0 ? (', up to '.$decimal.' decimal place'.($decimal == '1' ? '' : 's')) : '';
 	$this->setError($name, 'must be between '.$min.' and '.$max.$suf);
 }
-else if (strlen($decCheck[1]) > $decimal)
-{
-	if ($decimal == '0')
-	{
+else if (strlen($decCheck[1]) > $decimal) {
+	if ($decimal == '0') {
 		$this->setError($name, 'cannot be decimal');
 	}
-	else
-	{
+	else {
 		$this->setError($name, 'must be no more than '.$decimal.' decimal place'.($decimal == '1' ? '' : 's'));
 	}
 }
-else if ($step != 'any')
-{
-	if (fmod($min, $step) == 0 && fmod($input, $step) != 0)
-	{
-		// step was incorrect - minimum value CAN be cleanly divided by step
-		$this->setError($name, 'must be divisible by '.$step);
-	}
-	else if (fmod($min, $step) != 0 && fmod($input - $min, $step) != 0)
-	{
-		// step was incorrect - minimum value CANNOT be cleanly divided by step
-		$this->setError($name, 'must be in steps of '.$step.', starting at '.$min);
-	}
+else if ($step != 'any' && fmod($min, $step) == 0 && fmod($input, $step) != 0) {
+	// step was incorrect - minimum value CAN be cleanly divided by step
+	$this->setError($name, 'must be divisible by '.$step);
 }
-else
-{
+else if ($step != 'any' && fmod($min, $step) != 0 && fmod($input - $min, $step) != 0) {
+	// step was incorrect - minimum value CANNOT be cleanly divided by step
+	$this->setError($name, 'must be in steps of '.$step.', starting at '.$min);
+}
+else {
 	$this->field[$name]['valueclean'] = $input;
 }
